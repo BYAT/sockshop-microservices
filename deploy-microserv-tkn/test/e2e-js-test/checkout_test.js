a95027016c711d0581626f08875f4b1c9730eca2
Ubuntu <ubuntu@ip-10-0-1-235.me-south-1.compute.internal>
2020-10-23 21:21:12 +0000

    14 tests passed 2 failed

--- deploy-microserv-tkn/test/e2e-js-test/checkout_test.js
+++ deploy-microserv-tkn/test/e2e-js-test/checkout_test.js
@@ -35,9 +35,9 @@
 
     // go to the shopping cart
     casper.then(function() {
-      this.waitForText("1 item(s) in cart", function() {
+      this.waitForText("2 item(s) in cart", function() {
         test.pass("cart is updated with one product");
-        this.clickLabel("1 item(s) in cart");
+        this.clickLabel("2 item(s) in cart");
       }, function() {
         test.fail("cart was not updated");
       }, 3000);
@@ -65,7 +65,8 @@
 
     // actually checkout
     casper.then(function() {
-      this.waitForText("My orders", function() {
+      this.waitForText("view", function() {
+        test.comment("i am here");
         test.pass("user is taken to the orders page");
       }, function() {
         console.log("dumping page screenshot as PNG")
