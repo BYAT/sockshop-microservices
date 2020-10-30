cb3028dac032323b8370986edd1f9f4d69bd5211
Ubuntu <ubuntu@ip-10-0-1-38.me-south-1.compute.internal>
2020-10-30 00:58:04 +0000

    clean repo & add grafana and elf

--- monitoring/elf/elf.sh
+++ monitoring/elf/elf.sh
@@ -3,8 +3,8 @@
 helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
-helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=logging --values toleration.yaml
+helm install elasticsearch elastic/elasticsearch --version=7.9.3 --namespace=logging --values toleration.yaml
 helm install fluent-bit fluent/fluent-bit --namespace=logging --values toleration.yaml
-helm install kibana elastic/kibana --version=7.9.0 --namespace=logging --set service.type=LoadBalancer --values toleration.yaml
+helm install kibana elastic/kibana --version=7.9.3 --namespace=logging --set service.type=LoadBalancer --values toleration.yaml
 kubectl run random-logger --image=chentex/random-logger -n logging --values toleration.yaml
 kubectl apply -f ingress.yaml -n logging
